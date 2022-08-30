fcb1124d2c6633e3c22c1557fde470bd89bf6db0
willem van Es <wjc.van.es@gmail.com>
2022-08-30 17:40:08 +0200

    Fixing security/generate-keystore.sh & generating all stores for all brokers.

--- security/generate-keystore.sh
+++ security/generate-keystore.sh
@@ -1,7 +1,7 @@
 COMMON_NAME=$1
 ORGANIZATIONAL_UNIT="Community"
 ORGANIZATION="Pluralsight"
-CITY="Utah"
+CITY="Salt Lake City"
 STATE="Utah"
 COUNTRY="US"
 
@@ -10,21 +10,38 @@ CA_CERT_FILE="ca-cert"
 VALIDITY_DAYS=36500
 
 # Generate Keystore with Private Key
-keytool -keystore keystore/$COMMON_NAME.keystore.jks -alias $COMMON_NAME -validity $VALIDITY_DAYS -genkey -keyalg RSA -dname "CN=$COMMON_NAME, OU=$ORGANIZATIONAL_UNIT, O=$ORGANIZATION, L=$CITY, ST=$STATE, C=$COUNTRY"
+echo "Commencing keystore/$COMMON_NAME.keystore.jks generation..."
+keytool -keystore keystore/$COMMON_NAME.keystore.jks -alias $COMMON_NAME -validity $VALIDITY_DAYS -genkey -keyalg RSA \
+-dname "CN=$COMMON_NAME, OU=$ORGANIZATIONAL_UNIT, O=$ORGANIZATION, L=$CITY, ST=$STATE, C=$COUNTRY"
+wait
+echo "Generation of keystore/$COMMON_NAME.keystore.jks completed."
 
 # Generate Certificate Signing Request (CSR) using the newly created KeyStore
-keytool -keystore keystore/$COMMON_NAME.keystore.jks -alias $COMMON_NAME -certreq -file $COMMON_NAME.csr 
+echo "Commencing $COMMON_NAME.csr generation..."
+keytool -keystore keystore/$COMMON_NAME.keystore.jks -alias $COMMON_NAME -certreq -file $COMMON_NAME.csr
+wait
+echo "Generation of $COMMON_NAME.csr completed."
 
 # Sign the CSR using the custom CA
-openssl x509 -req -CA ca-cert -CAkey ca-key -in $COMMON_NAME.csr -out $COMMON_NAME.signed -days $VALIDITY_DAYS -CAcreateserial
+echo "Commencing $COMMON_NAME.signed generation by signing $COMMON_NAME.csr with $CA_CERT_FILE..."
+openssl x509 -req -CA $CA_CERT_FILE -CAkey ca-key -in $COMMON_NAME.csr -out $COMMON_NAME.signed -days $VALIDITY_DAYS -CAcreateserial
+wait
+echo "Generation of $COMMON_NAME.signed completed."
 
 # Import ROOT CA certificate into Keystore
+echo "Commencing import of $CA_CERT_FILE into keystore/$COMMON_NAME.keystore.jks..."
 keytool -keystore keystore/$COMMON_NAME.keystore.jks -alias $CA_ALIAS -importcert -file $CA_CERT_FILE
+wait
+echo "Import of $CA_CERT_FILE into keystore/$COMMON_NAME.keystore.jks completed."
 
 # Import newly signed certificate into Keystore
+echo "Commencing import $COMMON_NAME.signed into keystore/$COMMON_NAME.keystore.jks..."
 keytool -keystore keystore/$COMMON_NAME.keystore.jks -alias $COMMON_NAME -importcert -file $COMMON_NAME.signed
+wait
+echo "Import $COMMON_NAME.signed into keystore/$COMMON_NAME.keystore.jks completed."
 
 # Clean-up 
 rm $COMMON_NAME.csr
 rm $COMMON_NAME.signed
-rm ca-cert.srl
+# This is never created
+# rm ca-cert.srl
--- security/generate-truststore.sh
+++ security/generate-truststore.sh
@@ -3,4 +3,5 @@ CA_ALIAS="ca-root"
 CA_CERT_FILE="ca-cert" 
 
 #### Generate Truststore and import ROOT CA certificate ####
+echo "Creating truststore/$INSTANCE.truststore.jks and importing $CA_CERT_FILE into it ..."
 keytool -keystore truststore/$INSTANCE.truststore.jks -import -alias $CA_ALIAS -file $CA_CERT_FILE
